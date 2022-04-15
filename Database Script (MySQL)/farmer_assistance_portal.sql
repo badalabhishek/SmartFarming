3e1a12c07ed247f5e7dd7f19d14cf67e3e8db1b9
Abhishek Badal <badalabhishek@outlook.com>
2022-04-15 14:52:03 +0530

    w-e message desired

--- Database Script (MySQL)/farmer_assistance_portal.sql
+++ Database Script (MySQL)/farmer_assistance_portal.sql
@@ -1,22 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.4.3
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jun 14, 2020 at 07:34 AM
--- Server version: 5.7.20
--- PHP Version: 7.2.27
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
---
 -- Database: `farmer_assistance_portal`
 --
 
